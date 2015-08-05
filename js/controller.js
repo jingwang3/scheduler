a487f5a91bb5da37afa5eb55da4b3ec056e89c7d
Jing Wang <jingwang3@gmail.com>
2015-08-05 21:31:50 +0000

    click and modal

--- js/controller.js
+++ js/controller.js
@@ -1,29 +1,66 @@
 angular.module('scheduleApp', [])
   .controller('MainController', function($scope) {
-    $scope.schedules = [
-      {
-        timeframe:'8-9',
-        times:[
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          0
+    $scope.schedules = [{
+      timeframe: '8-9',
+      times: [{
+        open: 1,
+        users: [
+          {name: "Jing"}
         ]
-      },
-      {
-        timeframe:'9-10',
-        times:[
-          1,
-          1,
-          1,
-          1,
-          1,
-          0,
-          0
-        ]
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 0,
+        users: []
+      }]
+    }, {
+      timeframe: '9-10',
+      times: [{
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 1,
+        users: []
+      }, {
+        open: 0,
+        users: []
+      }, {
+        open: 0,
+        users: []
+      }]
+    }];
+
+    $scope.openTimeframe = function(i, j, time) {
+      if (time.open) {
+        $scope.currentTimeFrame = {
+          x: i,
+          y: j,
+          users: time.users
+        };
+        
+        $('#myModal').modal('show');
       }
-    ]
+    };
   });
