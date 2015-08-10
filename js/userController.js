093a25706c6976d479781dd3b72beea448646b1a
Jing Wang <jingwang3@gmail.com>
2015-08-10 17:42:42 +0000

    user functionalities

--- js/userController.js
+++ js/userController.js
@@ -5,6 +5,17 @@ angular.module('scheduleApp', [])
     $scope.activeUser;
     $scope.userIsEdited = false;
     
+    $http.get('http://test.misaka.us/db_driver/get_people.php').
+      then(function(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.users = response.data;
+      }, function(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(response.status);
+      });    
+
     $scope.selectUser = function(user){
       $scope.activeUser = user;
       $scope.selectedUser = angular.copy(user);
@@ -29,11 +40,19 @@ angular.module('scheduleApp', [])
     };
     
     $scope.saveUserEdits = function(){
-      for (var prop in $scope.activeUser) {
-          if ($scope.activeUser.hasOwnProperty(prop)) {
-              $scope.activeUser[prop] = $scope.selectedUser[prop];
-          }
-      };
+      $http.post('http://test.misaka.us/db_driver/add_person.php', $scope.activeUser).
+              then(function(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                for (var prop in $scope.activeUser) {
+                    if ($scope.activeUser.hasOwnProperty(prop)) {
+                        $scope.activeUser[prop] = $scope.selectedUser[prop];
+                    }
+                };
+              }, function(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+              });
     };
     
     $scope.addUser = function(){
